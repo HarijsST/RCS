7568672d4baab6253f5b157d256ba3a16360dd4d
HarijsST <42386020+HarijsST@users.noreply.github.com>
2018-09-03 20:59:53 +0300

    CSharp

---
+++ C#/Day1/Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+            int veselsSkaitlis = 5;
+            double arKomatu = 4.5;
+            String simboluVirkne = "definejas sadi";
+            char simbols = 'g';
+            bool patiessVaiNe = true;
+            veselsSkaitlis = 6;
+
+            // Console.WriteLine("Ievadiet vardu");
+            // simboluVirkne = Console.ReadLine();
+            // Console.WriteLine("Sveiki, " + simboluVirkne);
+
+            /*Console.WriteLine("Ievadiet skaitli");
+            simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis = Convert.ToInt16(simboluVirkne);
+            Console.WriteLine(jaunaisSkaitlis);
+            */
+            
+
+
+            Console.WriteLine("Ievadiet skaitli");
+            simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis = Convert.ToInt16(simboluVirkne);
+
+            Console.WriteLine("Ievadiet skaitli 2");
+            simboluVirkne = Console.ReadLine();
+            int jaunaisSkaitlis2 = Convert.ToInt16(simboluVirkne);
+            Console.WriteLine(jaunaisSkaitlis + jaunaisSkaitlis2);
+            
+
+
+
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
